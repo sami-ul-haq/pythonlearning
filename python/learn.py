f3c09743596c70f21b59067315e763c1d0f47e99
Sami ul Haq <49587542+samiulhaq1810@users.noreply.github.com>
2019-04-20 21:55:39 +0500

    1st Change

---
+++ python/learn.py
@@ -0,0 +1,33 @@
+# 50
+# While Loop Practice
+# i= 1
+# while i<=10:
+#     print(f"Sami ul Haq {i}")
+#     i = i + 1
+
+#51
+# total = 0
+# i = 1
+# while i <= 10:
+#     total += i
+#     i += 1
+
+# print(total)
+
+# n= input("Enter Number: ")
+# total = 0
+# i = 0
+# while i < len(n):
+#     total += int(n[i])
+#     i += 1
+# print(total)
+
+# class 56
+# name = input("Enter Ypur Name: ")
+# temp = ""
+# i = 0
+# while i < len(name):
+#     if name[i] not in temp:
+#         temp += name[i]
+#         print(f"{name[i]} : {name.count(name[i])}")
+#     i += 1
